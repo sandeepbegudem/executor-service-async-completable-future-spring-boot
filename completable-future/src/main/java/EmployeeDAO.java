5d9f7811d146171008a230e01d9e247d5aba6995
Sandeep B <124546985+CodingWithSandeepB@users.noreply.github.com>
2023-11-06 14:40:55 -0600

    Update EmployeeDAO.java

--- completable-future/src/main/java/EmployeeDAO.java
+++ completable-future/src/main/java/EmployeeDAO.java
@@ -1,1 +1,26 @@
-import com.fasterxml.jackson.core.type.TypeReference;import com.fasterxml.jackson.databind.DeserializationFeature;import com.fasterxml.jackson.databind.ObjectMapper;import dto.Employees;import java.io.File;import java.io.IOException;import java.util.List;public class EmployeeDAO {    public static List<Employees> fetchEmployees(){        ObjectMapper mapper = new ObjectMapper();        mapper.disable(DeserializationFeature                .FAIL_ON_UNKNOWN_PROPERTIES);        File file = new File(System.getProperty("user.home"), "Downloads");        try {           return mapper.readValue(new File(file, "employees.json"), new TypeReference<List<Employees>>() {                    });        } catch (IOException e) {            throw new RuntimeException(e);        }    }}
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.Employees;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class EmployeeDAO {
+
+
+    public static List<Employees> fetchEmployees(){
+      
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature
+                .FAIL_ON_UNKNOWN_PROPERTIES);
+        File file = new File(System.getProperty("user.home"), "Downloads");
+        try {
+           return mapper.readValue(new File(file, "employees.json"), new TypeReference<List<Employees>>() {
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
